a6548ba50547c4c38201d04500dafa905e102002
田原大貴 <taharadaiki@taharadaikinoMacBook-puro.local>
2019-11-16 15:45:45 +0900

    fixed

---
+++ application/views/common/footer.php
@@ -0,0 +1,21 @@
+<div class="footer">
+  <a href="<?= base_url('statics/index'); ?>">
+    <div class="footer-logo">
+      <img src="<?= base_url('static/images/component-02.png'); ?>">
+    </div>
+  </a>
+
+  <div class="footer-menu">
+    <ul>
+        <li><a href="<?= base_url('statics/about'); ?>">会社情報</a></li>
+        <li><a href="<?= base_url('statics/roomIndex'); ?>">実績</a></li>
+        <li><a href="<?= base_url('statics/fee'); ?>">料金例</a></li>
+        <li><a href="<?= base_url('statics/flow'); ?>">フロー</a></li>
+        <li><a href="<?= base_url('contact/index'); ?>">お問い合わせ</a></li>
+    </ul>
+  </div>
+
+  <div class="footer-copyright">
+    <p>©LUPINUS 2019 ALL RIGHT RESERVED</p>
+  </div>
+</div>
